2de39b3f03d9892fd761f8969a47a8180559322f
David Leen <dleen@uw.edu>
2013-03-07 18:05:32 -0800

    windows

--- runscala.sh
+++ runscala.sh
@@ -1,1 +1,4 @@
 time java -Xmx2048M -Xss1m -cp .:/usr/local/Cellar/scala/2.10.0/libexec/lib/scala-library.jar -Dpar=1 main.scala.Hogwild.BigDataProcessor
+
+# Windows
+java -Xmx4000M -cp "C:\Program Files (x86)\scala\lib\scala-library.jar";. -Dpar=6 main.scala.Hogwild.BigDataProcessor 5
